720217ea383c238a766d20f4084fc210f3e058fc
emanuelmath <emanueldominguezm4@gmail.com>
2026-01-04 19:23:37 -0600

    Initial clean architecture structure

---
+++ Fiducia.Domain/Entities/Loan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiducia.Domain.Enums;
+
+namespace Fiducia.Domain.Entities
+{
+    public class Loan
+    {
+        public Guid Id { get; set; }
+        public Guid ClientId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal InterestRate { get; set; }
+        public int TermMonths { get; set; }
+        public LoanStatus Status { get; set; }
+        public DateTime RequestDate { get; set; } 
+        public DateTime? DisbursementDate { get; set; } 
+        public DateTime? FinishedDate { get; set; } 
+    }
+}
---
+++ Fiducia.Domain/Enums/LoanStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Fiducia.Domain.Enums
+{
+    public enum LoanStatus
+    {
+        Pending = 1,
+        Approved = 2,     
+        Rejected = 3,     
+        Disbursed = 4,    
+        Completed = 5,    
+        Cancelled = 6
+    }
+}
